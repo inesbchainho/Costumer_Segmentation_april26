e1c02fc9fe0efc624b11ccb8eddb927c0f86805f
inesbchainho <inesbchainho@gmail.com>
2026-06-08 21:00:38 +0100

    more stuff

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,2 +1,49 @@
+import pandas as pd
+import numpy as np
+import re
+
 def preprocess_customer_info(df):
+    """
+    Cleans customer_info and creates additional features for customer segmentation.
+    """
+    df_clean = df.copy()
+
+    def extract_degree(name):
+        match = re.match(r'^(Bsc|Msc|Phd)\.', str(name))
+        return match.group(1) if match else 'None'
+
+    df_clean['degree'] = df_clean['customer_name'].apply(extract_degree)
+    df_clean['degree_num'] = df_clean["degree"].map({'Bsc': 1, 'Msc': 2, 'Phd': 3, 'None': 0})
+
+    df_clean["is_female"] = (df_clean["customer_gender"] == "female").astype(int)
     
+    df_clean["customer_birthdate"] = pd.to_datetime(df_clean["customer_birthdate"], format="%m/%d/%Y %I:%M %p", errors="coerce")
+    reference_date = pd.Timestamp("2026-06-01")
+    df_clean["age"] = ((reference_date - df_clean["customer_birthdate"]).dt.days / 365.25)
+    df_clean.loc[(df_clean["age"] < 16) | (df_clean["age"] > 100), "age"] = np.nan
+
+    df_clean.loc[df_clean["year_first_transaction"] > 2026, "year_first_transaction"] = np.nan
+    df_clean["tenure"] = 2026 - df_clean["year_first_transaction"]
+
+    df_clean["has_loyalty_card"] = df_clean["loyalty_card_number"].notna().astype(int) # we assume the null values are the lack of a loyalty card
+
+    df_clean.loc[df_clean["percentage_of_products_bought_promotion"] < 0, "percentage_of_products_bought_promotion"] = np.nan
+
+    df_clean = df_clean.drop(columns=["customer_name", "degree", "customer_gender", "customer_birthdate", "year_first_transaction", "loyalty_card_number"])
+    
+    return df_clean
+    
+
+def fill_missing_values(df_clean):
+    numeric_cols = df_clean.select_dtypes(include = ["number"]).columns
+
+    for col in numeric_cols:
+        df_clean[col] = df_clean[col].fillna(df_clean[col].median())
+    
+    spend_cols = ["lifetime_spend_groceries", "lifetime_spend_electronics", "lifetime_spend_vegetables", "lifetime_spend_nonalcohol_drinks", "lifetime_spend_alcohol_drinks",
+                  "lifetime_spend_meat", "lifetime_spend_fish", "lifetime_spend_hygiene", "lifetime_spend_videogames", "lifetime_spend_petfood"]
+    
+    df_clean["total_lifetime_spend"] = df_clean[spend_cols].sum(axis=1)
+    
+    df_clean = df_clean.replace([np.inf, -np.inf], np.nan)
+    return df_clean
